82d64fdf782ebb0bdc08c7025e9413ea45334372
Carrie Savinell <carrielane89@gmail.com>
2018-07-31 15:59:52 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,25 @@
 class SchoolClassesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(paraks[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student = Student.update(params[:student])
+    redirect_to student_path(@student)
+  end
 end
